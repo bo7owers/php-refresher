6253588cf99b3c8f71a7c97ae229e75f18fe8dc8
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-19 13:28:42 -0500

    Create function dump and die

--- page-links/index.php
+++ page-links/index.php
@@ -1,5 +1,13 @@
 <?php
 $heading = 'Home';
 
+// Create function dump and die
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
 
 require "./views/index.view.php";
