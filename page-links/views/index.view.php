b0c7090256b3e5aa9d5cbaba019d0268c08f2ba9
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-19 13:07:34 -0500

    update view content

--- page-links/views/index.view.php
+++ page-links/views/index.view.php
@@ -5,7 +5,7 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Replace with your content -->
-        <p>my content</p>
+        <p>Home page</p>
         <!-- /End replace -->
     </div>
 </main>
