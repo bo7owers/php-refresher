012934d615bc761af1c48c67638a1e7f3b7a6a1f
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-19 13:44:05 -0500

    Clean up index.php

--- page-links/index.php
+++ page-links/index.php
@@ -1,13 +1,8 @@
 <?php
+require('functions.php');
+
 $heading = 'Home';
 
-// Create function dump and die
-function dd($value)
-{
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-    die();
-}
+
 
 require "./views/index.view.php";
