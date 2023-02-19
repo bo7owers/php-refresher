2ee6bd748c121ed23cbe28c584b6382de877bb91
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-19 13:06:30 -0500

    Create banner partial

--- page-links/views/about.view.php
+++ page-links/views/about.view.php
@@ -1,13 +1,6 @@
 <?php require('partials/head.php'); ?>
-
 <?php require('partials/nav.php'); ?>
-<header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-            About
-        </h1>
-    </div>
-</header>
+<?php require('partials/banner.php'); ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Replace with your content -->
