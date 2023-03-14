1e1dc96b57a6b0a2cb92f0296e09eabd5421988a
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-03-13 23:17:16 -0400

    tweak nav

--- page-links/views/partials/nav.php
+++ page-links/views/partials/nav.php
@@ -9,15 +9,15 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/learning-php/page-links/"
-                            class="<?= urlIs('/learning-php/page-links/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>  hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        <a href="/learning-php/page-links"
+                            class="<?= urlIs('/learning-php/page-links') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>  hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                             aria-current="page">Home</a>
 
-                        <a href="/learning-php/page-links/about.php"
-                            class="<?= $_SERVER['REQUEST_URI'] === '/learning-php/page-links/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+                        <a href="/learning-php/page-links/about"
+                            class="<?= $_SERVER['REQUEST_URI'] === '/learning-php/page-links/about' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
 
-                        <a href="/learning-php/page-links/contact.php"
-                            class="<?= $_SERVER['REQUEST_URI'] === '/learning-php/page-links/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+                        <a href="/learning-php/page-links/contact"
+                            class="<?= $_SERVER['REQUEST_URI'] === '/learning-php/page-links/contact' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
                     </div>
                 </div>
             </div>
