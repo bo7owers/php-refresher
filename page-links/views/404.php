2c682c7826efe5d434cf3beaf03dc15493614f4b
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-03-13 23:36:41 -0400

    create 404

---
+++ page-links/views/404.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php'); ?>
+<?php require('partials/nav.php'); ?>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <h1 class="text-dark-900 text-xl font-extrabold">Sorry, not found.</h1>
+
+        <div class="mt-4"><a href="/learning-php/page-links" class="text-blue-600 underline underline-blue-600">Go back
+                home</a></div>
+        <!-- /End replace -->
+    </div>
+</main>
